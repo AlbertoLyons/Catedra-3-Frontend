f308e0026caff8278bef7fcc18c5abb75d68d495
AlbertoLyons <129765764+AlbertoLyons@users.noreply.github.com>
2025-01-07 13:39:46 -0300

    feat: added interceptor, guard and LLS

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './interceptors/jwt';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(withFetch(), withInterceptors([jwtInterceptor])),
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes)
+  ]
 };
